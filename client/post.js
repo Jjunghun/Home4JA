45fceb35885bca6f7841fb587d4176b20755fad4
Jjunghun <wernilia@gmail.com>
2016-06-06 01:06:10 +0900

    form 태그 제거 후 input 태그만 사용, 코드 정리

--- client/post.js
+++ client/post.js
@@ -1,20 +1,15 @@
 Template.post.events({
-    "submit .new-title": function (event) {
-      event.preventDefault();
+    'click .confirm': function () {
+        var contentsInfo = {
+            //title : event.target.title.value,
+            title : $("#new-title").val(),
+            content : $("#new-content").val()
+        };
 
-      var contentsInfo = {
-          title : event.target.title.value,
-          content : $("#content").val()
-      };
+        console.log(contentsInfo);
 
-      console.log(contentsInfo);
+        Meteor.call("addPost", contentsInfo);
 
-      Meteor.call("addPost", contentsInfo);
-        
-      event.target.title.value = "";
-    },
-
-    'click #confirm': function () {
         Router.go('/');
     }
 });
