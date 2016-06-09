b0f3fc9c9ef616cf48dd014c660db1d5aff522a5
KwakHongGeun <kwak5674@naver.com>
2016-06-09 22:59:48 +0900

    login 화면 DB저장 1차 확인

--- client/main.js
+++ client/main.js
@@ -4,7 +4,7 @@ Router.configure({
 
 Router.route('/', {name: 'main'});
 Router.route('/post', {name: 'post'});
-
+Router.route('/complete', {name: 'complete'});
 // Router.route('/post', function () {
 //    this.render('post');
 // });
@@ -12,6 +12,49 @@ Router.route('/post', {name: 'post'});
 Template.main.events({
     'click #post' : function () {
         Router.go('/post');
+
+    },
+    'click .message a':function(){
+        $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+    },
+    'click  #login':function(event){
+        event.preventDefault();
+        var name = event.target.NAME.value;
+        var password = event.target.PSW.value;
+
+        Meteor.loginWithPassword(email, password, function(err){
+            if (err){}
+            // The user might not have been found, or their passwword
+            // could be incorrect. Inform the user that their
+            // login attempt has failed.
+            else{}
+            // The user has been logged in.
+        });
+        return false;
+    },
+    'click .register-form #create':function(event) {
+        event.preventDefault();
+        //var name = event.target.NAME.value;
+        //var password = event.target.PSW.value;
+        //var email = event.target.EMAIL.value;
+
+        var loginInfo = {
+          name : $("#name").val(),
+          password: $("#psw").val(),
+          email: $("#email").val()
+       };
+
+        console.log(loginInfo);
+
+
+
+        //Accounts.createUser({name: name, password: password, email: email},
+        //    function (err) {
+        //        if (err) {}
+        //        else {}
+        //    });
+        //
+        //return false;
     }
 });
 
