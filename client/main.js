090c15e996ffb9b683d5f6259d92bb1569fd523a
KwakHongGeun <kwak5674@naver.com>
2016-06-14 01:58:51 +0900

    about 페이지 구현

--- client/main.js
+++ client/main.js
@@ -1,81 +1,6 @@
 Session.set("target", "default");
 // Session.set(String key, any value);
 
-<<<<<<< HEAD
-Router.configure({
-<<<<<<< HEAD
-    mainTemplate: 'main'
-=======
-   layoutTemplate: 'layout'
->>>>>>> 17e1610318f93294307d6eea968b824fe3766ef9
-});
-
-Router.route('/', {name: 'main'});
-Router.route('/post', {name: 'post'});
-<<<<<<< HEAD
-Router.route('/complete', {name: 'complete'});
-Router.route('/about', {name: 'about'});
-// Router.route('/post', function () {
-//    this.render('post');
-// });
-
-Template.main.events({
-    'click #post' : function () {
-        Router.go('/post');
-    },
-    'click #about':function() {
-        Router.go('/about')
-    },
-    'click .message a':function(){
-        $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
-    },
-    'click  #login':function(event){
-        event.preventDefault();
-        var name = event.target.NAME.value;
-        var password = event.target.PSW.value;
-
-        Meteor.loginWithPassword(email, password, function(err){
-            if (err){}
-            // The user might not have been found, or their passwword
-            // could be incorrect. Inform the user that their
-            // login attempt has failed.
-            else{}
-            // The user has been logged in.
-        });
-        return false;
-    },
-    'click .register-form #create':function(event) {
-        event.preventDefault();
-        //var name = event.target.NAME.value;
-        //var password = event.target.PSW.value;
-        //var email = event.target.EMAIL.value;
-
-        var loginInfo = {
-          name : $("#name").val(),
-          password: $("#psw").val(),
-          email: $("#email").val()
-       };
-
-        console.log(loginInfo);
-=======
-Router.route('/login', {name: 'loginPage'});
-Router.route('/joinPage', {name: 'joinPage'});
-Router.route('/view/:_id', {
-    name: 'view',
-    data: function () {
-        var id = this.params._id;
-        return Posts.find({_id: id});
-    }
-});
->>>>>>> 17e1610318f93294307d6eea968b824fe3766ef9
-
-Template.layout.helpers({
-   toposts: function () {
-       return Posts.find({});
-   }
-});
-
-=======
 
 Template.main.helpers({
     toposts: function () {
@@ -83,18 +8,8 @@ Template.main.helpers({
     }
 });
 
-<<<<<<< HEAD
-Template.main.helpers({
-   toposts: function () {
-       console.log(Posts.find({}));
-       return Posts.find({});
-   } 
-});
-=======
->>>>>>> test1
 Template.view.helpers({
     toposts: function () {
         return Posts.find({'contentsInfo.title' : Session.get("target")});
     }
 });
->>>>>>> 17e1610318f93294307d6eea968b824fe3766ef9
--- client/router.js
+++ client/router.js
@@ -15,3 +15,17 @@ Router.route('/view/:_id', {
         return Posts.find({_id: id});
     }
 });
+
+Router.route('/about', {name: 'about'});
+Router.route('/intro', {name: 'intro'});
+Router.route('/rules', {name: 'rules'});
+Router.route('/tips', {name: 'tips'});
+Router.route('/list', {name: 'list'});
+
+
+    Template.about.events({
+    'click #boot':function (evt) {
+        $('html, body').animate({scrollTop: $("#tips").offset().top}, 0);
+        $('#tips').css('margin-bottom',750);
+    }
+});
